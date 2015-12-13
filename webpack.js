0dceadab4271f050bf6b9abb4d22d8ca72fefec0
Denis Hananein <i@zloylos.me>
2015-12-13 14:43:06 +0300

    Add compress

--- webpack.js
+++ webpack.js
@@ -1,7 +1,15 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: './plugin.js',
     output: {
         path: './',
         filename: 'shower-mirror.js'
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {warnings: false},
+            output: {comments: false}
+        })
+    ]
 };
